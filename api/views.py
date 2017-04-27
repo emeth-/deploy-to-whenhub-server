81cce70d58b49eafafaef24e7f7ec523586f0a53
emeth- <seanybob@gmail.com>
2017-04-27 09:07:25 -0500

    sweet

--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ import json
 from api.models import Widget
 from django.http import HttpResponseRedirect
 from django.template.response import TemplateResponse
+import github_parser
+import whenhub_api
 
 def json_custom_parser(obj):
     if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.date):
@@ -18,10 +20,24 @@ def load_frontend(request):
     return HttpResponseRedirect("/static/index.html")
 
 def load_widget(request):
-    print "*****", request.META.get('HTTP_REFERER')
-    print request.META
-    return TemplateResponse(request, 'load_widget.html', context={
-        "users": []
+    refer = request.META.get('HTTP_REFERER')
+    if not refer:
+        return TemplateResponse(request, 'error.html', context={
+            "error": "Referred not detected."
+        })
+
+    #TODO make it work if it's the readme. This assumes it's not the readme.
+
+    #refer = https://github.com/emeth-/the-flow/blob/master/mary_perpetual_virginity.md
+    refer = refer.replace('/blob', '')
+    refer = refer.replace('github.com', 'raw.githubusercontent.com')
+    #refer = https://raw.githubusercontent.com/emeth-/the-flow/master/mary_perpetual_virginity.md
+
+    github_data = github_parser.parse_github_url(refer)
+    schedule_id = whenhub_api.create_widget(github_data)
+
+    return TemplateResponse(request, 'horizontal_timeline.html', context={
+        "schedule_id": schedule_id
     })
 
 def create_widget(request):
--- whenhub_api.py
+++ whenhub_api.py
@@ -1,38 +1,15 @@
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'hackathon.settings'
 import hackathon.settings
-import django
-django.setup()
 
-import json
 import requests
-import github_parser
 
-github_data = github_parser.parse_github_url("https://raw.githubusercontent.com/emeth-/the-flow/master/mary_perpetual_virginity.md")
+def create_widget(github_data):
 
-resp = requests.post("https://api.whenhub.com/api/users/me/schedules?access_token="+hackathon.settings.WHENHUB_ACCESS_TOKEN, json=github_data['schedule'])
-schedule_data = resp.json()
+    resp = requests.post("https://api.whenhub.com/api/users/me/schedules?access_token="+hackathon.settings.WHENHUB_ACCESS_TOKEN, json=github_data['schedule'])
+    schedule_data = resp.json()
 
-whenhub_url = "https://studio.whenhub.com/schedules/"+schedule_data['id']
+    for event_data in github_data['events']:
+        resp = requests.post("https://api.whenhub.com/api/schedules/"+schedule_data['id']+"/events?access_token="+hackathon.settings.WHENHUB_ACCESS_TOKEN, json=event_data)
+        #event_data = resp.json()
 
-for event_data in github_data['events']:
-    resp = requests.post("https://api.whenhub.com/api/schedules/"+schedule_data['id']+"/events?access_token="+hackathon.settings.WHENHUB_ACCESS_TOKEN, json=event_data)
-    #event_data = resp.json()
-
-print "whenhub_url", whenhub_url
-
-#https://api.whenhub.com/api/schedules/<schedule_id>/events
-
-#print json.dumps(resp.json(), indent=4)
-
-"""
---The plan--
-
-input = mode (vertical, horizontal)
-- api call to create schedule
-- create events one by one
-- redirect user to widget
-- add to local WIDGETS table
-- environment variable with api key
-
-"""
+    return schedule_data['id']
+    #return "https://studio.whenhub.com/schedules/"+schedule_data['id']
